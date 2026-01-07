2e538abf2f12cf6243e8420398770faef3041d99
Slmpire <sioplightman5@gmail.com>
2026-01-08 00:10:52 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/daily-code-journal/sw.js', { scope: '/daily-code-journal/' })})}
